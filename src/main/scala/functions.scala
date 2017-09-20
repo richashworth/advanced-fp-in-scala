32587a6c5902e8153bb4d839ba49693fe7343a58
Richard Ashworth <richardashworth575@gmail.com>
2017-09-20 11:27:08 +0100

    Implemented first exercise

--- src/main/scala/functions.scala
+++ src/main/scala/functions.scala
@@ -25,10 +25,19 @@ object exercise1 {
   case object Neutral extends Attitude
   case object Dislike extends Attitude
 
+  val preference: Groceries => Attitude = (g: Groceries) =>
+    g match {
+      case Vegetables => Love
+      case Fruits     => Like
+      case Meat       => Neutral
+      case Dairy      => Like
+      case Eggs       => Dislike
+  }
   // preference(Eggs)
 }
 
 object exercise2 {
+
   /** "Fresh Food" sells Apples and Oranges */
   sealed trait Item
   case object Apple extends Item
@@ -36,22 +45,28 @@ object exercise2 {
 
   /** They calculate price for items */
   val price: Item => Double = {
-    case Apple => 3
+    case Apple  => 3
     case Orange => 4
   }
 
   /** And apply some discounts from time to time */
   val discount: Item => Double = {
-    case Apple => 0.1
+    case Apple  => 0.1
+    case Orange => 0.0
   }
 
-  /** 
+  /**
     * Write function finalPrice that will calculate the final price for the item by
     * applying dicount to initial price
-    * 
+    *
     * Call the function for both Apples and Oranges. Is everything in order? How can you fix it?
     */
-  val finalPrice: Item => Double = ???
+  val finalPrice: Item => Double = (i: Item) => {
+    val p = price(i)
+    val d = discount(i)
+    p - (p * d)
+  }
+
   finalPrice(Apple)
   finalPrice(Orange)
 }
@@ -65,20 +80,23 @@ object exercise3 {
   import exercise1._
 
   val rateComment: String => Attitude = (str: String) =>
-    if(str.contains("Scala")) Love else Neutral
+    if (str.contains("Scala")) Love else Neutral
 
   val prize: Attitude => Groceries = {
     case Love => Vegetables
     case _    => Eggs
   }
 
-  val prizeForComment: String => Groceries = ???
+  val prizeForComment: String => Groceries = prize compose rateComment
+  // is equivalent to:
+  // val prizeForComment: String => Groceries = rateComment andThen prize
 }
 
 /**
   * Follow the origins of quantum physics!
   */
 object exercise4 {
+
   /** Cats are cute animals. */
   case class Cat(happiness: Int, alive: Boolean)
 
@@ -88,38 +106,52 @@ object exercise4 {
   case object Milk extends Food
 
   /**
-    * You can do a lot with a cat: pet it, feed it or even hiss on it.  
+    * You can do a lot with a cat: pet it, feed it or even hiss on it.
     * Your actions will alter its level of happiness
     */
   val pet: Cat => Cat =
     cat => cat.copy(happiness = cat.happiness + 2)
   val hiss: Cat => Cat =
     cat => cat.copy(happiness = cat.happiness - 10)
-  val feed: (Cat, Food) => Cat = {
-    case (c, Milk) => c.copy(happiness = c.happiness + 1)
-    case (c, Meat) => c.copy(happiness = c.happiness + 3)
+  val feed: (Food, Cat) => Cat = {
+    case (Milk, c) => c.copy(happiness = c.happiness + 1)
+    case (Meat, c) => c.copy(happiness = c.happiness + 3)
   }
 
   /** Schrodinger is a crazy fellow. He closed his cat in a box!
     * Now Schrodinger does not know whether the cat is dead or alive but he seems happy about it.
     */
-  class SchrodingerBox(private val cat: Cat)
+  class SchrodingerBox(private val cat: Cat) {
+    def interact(f: Cat => Cat) = {
+      if (cat.alive) {
+        f(cat)
+      } else new SchrodingerBox(cat)
+    }
+  }
   object SchrodingerBox {
     def apply(cat: Cat): SchrodingerBox =
-      if(cat.happiness <= 3)
+      if (cat.happiness <= 3)
         new SchrodingerBox(cat.copy(alive = false))
       else new SchrodingerBox(cat)
   }
+
+  val cat = Cat(4, true)
+  val box = new SchrodingerBox(cat)
+
   /**
     * He has no intentions of revealing if the cat is dead or alive, but if it is, Schrodinger
     * would like to feed it and pet it from time to time.
-    * 
+    *
     * Your goals:
     * 1. Fulfil mad scientist dream and write a method (in SchrodingerBox) called "interact"
     * that allows applying all `Cat => Cat` functions on a cat (as long as the cat is alive)
     * 2. Try running running your function with `pet` and `hiss` functions.
     * 3. What about feed function? Can we still feed a cat within a SchrodingerBox?
     */
+  box.interact(pet)
+  box.interact(hiss)
+  //RA => Use CURRYING to solve(3)
+  box.interact(feed.curried(Milk))
 }
 
 object exercise5 {
@@ -131,35 +163,45 @@ object exercise5 {
 
   /** They have different prices for London and different for the rest of United Kingdom */
   val priceLondon: Item => Double = {
-    case Apple => 3
+    case Apple  => 3
     case Orange => 4
   }
   val priceRestUK: Item => Double = {
-    case Apple => 2
+    case Apple  => 2
     case Orange => 3
   }
 
   /** They sometimes apply discounts for the items they sell */
   val mondayDiscount: Item => Double = {
-    case Apple => 0.1
+    case Apple  => 0.1
     case Orange => 0.0
   }
   val holidayDiscount: Item => Double = {
-    case Apple => 0.1
+    case Apple  => 0.1
     case Orange => 0.3
   }
   val noDicount: Item => Double = i => 0.0
 
-  /** 
+  /**
     * Create a function called createCheckout that crafts a checkout for a given shop
-    * 
+    *
     * createCheckout will:
     * 1. take as a parameter a strategy to calculate original price
     * 2. take as a parameter a strategy to apply discount
     * 3. return a function that will take an Item as parameter and return a final price for
     *    that item as a result
     */
-  val createCheckout: (Item => Double) => (Item => Double) => (Item => Double) = ???
+  // We could use the Applicative pattern
+  val createCheckout: (Item => Double) => (Item => Double) => (Item => Double) =
+    (priceStrategy: Item => Double) => { (discountStrategy: Item => Double) =>
+      { (i: Item) =>
+        {
+          val p = priceStrategy(i)
+          val d = discountStrategy(i)
+          p - (p * d)
+        }
+      }
+    }
 
   val checkout: Item => Double = createCheckout(priceLondon)(mondayDiscount)
 }
@@ -168,7 +210,13 @@ object exercise5 {
   * Implement compose, curry, uncurry
   */
 object exercise6 {
-  def compose[A, B, C](f: A => B, g: B => C): A => C = ???
-  def curry[A, B, C](f: (A, B) => C): A => B => C = ???
-  def uncurry[A, B, C](f:  A => B => C): (A, B) => C = ???
+  def compose[A, B, C](f: A => B, g: B => C): A => C = (a: A) => {
+    g(f(a))
+  }
+  def curry[A, B, C](f: (A, B) => C): A => B => C = (a: A) => { (b: B) =>
+    f(a, b)
+  }
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C = { (a: A, b: B) =>
+    f(a)(b)
+  }
 }
